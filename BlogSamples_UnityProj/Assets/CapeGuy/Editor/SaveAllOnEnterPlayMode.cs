24fce14f571cd740e269d59cbbf42e82385adc4d
Ben Nicholson <ben@capeguy.co.uk>
2018-02-01 10:26:39 +0000

    Upgrade project to Unity 2017.3.0p2

--- BlogSamples_UnityProj/Assets/CapeGuy/Editor/SaveAllOnEnterPlayMode.cs
+++ BlogSamples_UnityProj/Assets/CapeGuy/Editor/SaveAllOnEnterPlayMode.cs
@@ -11,41 +11,52 @@ using UnityEditor.SceneManagement;
 using System.Collections.Generic;
 using System.Linq;
 
-/// <summary>
-/// This script saves the current project and scene (if there is one) whenever the Unity editor enters play mode.
-/// For more information see https://capeguy.co.uk/2015/07/unity-auto-save-on-play-in-editor/.
-/// </summary>
-[InitializeOnLoad]
-public class SaveAllOnEnterPlayMode {
+namespace CapeGuy.Utils.EditorUtils {
 
-	static SaveAllOnEnterPlayMode ()
-	{
+	/// <summary>
+	/// This script saves the current project and scene (if there is one) whenever the Unity editor enters play mode.
+	/// For more information see https://capeguy.co.uk/2015/07/unity-auto-save-on-play-in-editor/.
+	/// </summary>
+	[InitializeOnLoad]
+	public class SaveAllOnEnterPlayMode {
 
-		EditorApplication.playmodeStateChanged = () =>
+		static SaveAllOnEnterPlayMode () {
+
+			#if UNITY_2017_2_OR_NEWER
+			EditorApplication.playModeStateChanged += (UnityEditor.PlayModeStateChange stateChange) => {
+				if (stateChange == UnityEditor.PlayModeStateChange.ExitingEditMode) {
+					OnLeaveEditMode ();
+				}
+			};
+			#else
+			EditorApplication.playmodeStateChanged = () => {
+				if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying) {
+					OnLeaveEditMode ();
+				}
+			};
+			#endif
+		}
+
+		static void OnLeaveEditMode () {
+			string currSceneName = SceneManager.GetActiveScene ().name;
+			if (!string.IsNullOrEmpty (currSceneName) && !currSceneName.StartsWith("InitTestScene")) {
+				Debug.Log ("Auto-Saving scenes and assets before entering play mode: " +
+				           string.Join(", ", openSceneNames.ToArray()));
+				EditorSceneManager.SaveOpenScenes ();
+			}
+			AssetDatabase.SaveAssets ();
+		}
+
+		static IList<string> openSceneNames
 		{
-
-			if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
-			{
-				Debug.Log ("Auto-Saving scene(s) and assets before entering play mode: " + string.Join(", ", openSceneNames.ToArray()));
-
-				EditorSceneManager.SaveOpenScenes();
-				AssetDatabase.SaveAssets();
+			get {
+				int numScenes = SceneManager.sceneCount;
+				IList<string> openScenes = new List<string> (numScenes);
+				for (int currSceneIndex = 0; currSceneIndex < numScenes; ++currSceneIndex) {
+					openScenes.Add(SceneManager.GetSceneAt(currSceneIndex).name);
+				}
+				return openScenes;
 			}
-
-		};
-
-	}
-
-	static IList<string> openSceneNames
-	{
-		get {
-			int numScenes = SceneManager.sceneCount;
-			IList<string> openScenes = new List<string> (numScenes);
-			for (int currSceneIndex = 0; currSceneIndex < numScenes; ++currSceneIndex) {
-				openScenes.Add(SceneManager.GetSceneAt(currSceneIndex).name);
-			}
-			return openScenes;
 		}
 	}
-
 }
